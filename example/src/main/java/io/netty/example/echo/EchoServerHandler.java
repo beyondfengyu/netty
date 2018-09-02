65f081a6bfa6b88589f3df7246ac27e4c0e88ab6
rock <594981878@qq.com>
2018-09-02 15:10:32 +0800

    add logger

--- example/src/main/java/io/netty/example/echo/EchoServerHandler.java
+++ example/src/main/java/io/netty/example/echo/EchoServerHandler.java
@@ -18,12 +18,16 @@ package io.netty.example.echo;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Handler implementation for the echo server.
  */
 @Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(EchoServerHandler.class);
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
@@ -38,6 +42,7 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         // Close the connection when an exception is raised.
+        logger.info("cause:" + cause.getMessage());
         cause.printStackTrace();
         ctx.close();
     }
